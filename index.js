949d98669b26c26a3d6d98279569b2e1971b9e3d
Code4Bread <iz@kaungmyat.tech>
2019-11-22 16:40:46 +0800

    updated text

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).send('Hello World');
+    res.status(200).send('Hello EBS');
 })
 
 app.listen(3000, () => { console.log('Server is up and running') });
