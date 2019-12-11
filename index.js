ebb3795f6b2683b29a30321fa7fa783c61cc1315
Code4Bread <iz@kaungmyat.tech>
2019-12-11 14:30:56 +0800

    database connection added to script

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true})
+  .then(() => console.log('Database is connected'))
+  .catch((err) => console.log(err.message));
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.status(200).send('Hello EBS');
-})
+});
 
 app.listen(8080, () => {
     console.log('Server is up and running')
